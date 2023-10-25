d27aad726e97fce5d952e0761ba5aca4a9219d72
Bakeloh <bakelohnoel@gmail.com>
2023-10-25 12:32:38 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide() {
+    let divide = 2000 / 100;
+    return divide;
+}
+
+
+const square = (num) => num * num;
+square(1);
+
+// const square = (num) => num * num;
+// square(5);
+
+const add = (num1, num2) => num1 + num2;
+add(6, 1);
